c4844cf2cd486eef717b91cd2aa231fb85dd09cc
shiv1994 <shivramx@gmail.com>
2017-04-06 18:32:46 -0400

    Changes.

--- src/system/HTMLCode.java
+++ src/system/HTMLCode.java
@@ -22,9 +22,9 @@ public class HTMLCode {
             "  <title>Bootstrap Example</title>\n" +
             "  <meta charset=\"utf-8\">\n" +
             "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
-            "  <link rel=\"stylesheet\" href=\"..Files/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css\">\n" +
-//            "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">"+
-            "  <script src=\"..Files/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.min.js\"> </script>\n"+
+//            "  <link rel=\"stylesheet\" href=\"..Files/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css\">\n" +
+            "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">"+
+//            "  <script src=\"..Files/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.min.js\"> </script>\n"+
             "  <script> function getSelectedElement(){\n" +
             "    return (document.getElementById(\"partyNames\").value);\n" +
             "  }</script>\n" +
